ada400d2a41af8a063c0be1f17193b276f893c11
Jaikishore <jaikishore@dlearners.in>
2024-12-04 18:25:21 +0530

    6th slide inprogress

---
+++ Assets/Script/Thumbnail5Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thumbnail5Controller : MonoBehaviour
+{
+    public GameObject spawnParent;
+    public GameObject[] puzzleParts;
+
+    void Start()
+    {
+        SpawnPuzzleObjects();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SpawnPuzzleObjects()
+    {
+        foreach (var item in puzzleParts)
+        {
+            var spawnedObj = Instantiate(item, spawnParent.transform);
+        }
+    }
+}
