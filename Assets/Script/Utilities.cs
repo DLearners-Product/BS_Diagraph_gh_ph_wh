188168a593ffc96421603bcbf9b65628fa7bffaa
Jaikishore <jaikishore@dlearners.in>
2024-12-17 18:31:44 +0530

    BSGS-3086 QC changes done and BSGS-3088 in progress

--- Assets/Script/Utilities.cs
+++ Assets/Script/Utilities.cs
@@ -59,7 +59,7 @@ public class Utilities : MonoGenericSingleton<Utilities>
     {
         Sequence sequence = DOTween.Sequence();
         sequence.Append(obj.DOMove(endPos, 0.5f));
-        sequence.Join(obj.DOScale(Vector3.one * 1.5f, 0.5f));
+        sequence.Join(obj.DOScale(Vector3.one * 1.25f, 0.5f));
         if(onCompleteCallBack != null)
             sequence.onComplete += onCompleteCallBack;
         sequence.Play();
